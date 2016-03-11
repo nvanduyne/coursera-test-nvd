a0ed38a08e77295774b0cd603e5ea05b9378b90a
Nick VanDuyne <njvanduyne@gmail.com>
2016-03-11 08:10:52 -0500

    module 4 1st upload

--- Assignments/module4-solution-starter/js/script.js
+++ Assignments/module4-solution-starter/js/script.js
@@ -82,9 +82,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  allCategoriesUrl, 
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitely setting the flag to get JSON from server processed into an object literal
+  homeHtmlURL, 
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  }, 
+  false); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
