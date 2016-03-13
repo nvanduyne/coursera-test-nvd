d073d95c70aab5b42a1fbcf2d2109ff3a9da0ec8
nvanduyne <njvanduyne@gmail.com>
2016-03-13 12:10:38 -0400

    12
    back to the error can't get past the issue

--- Assignments/module4-solution-starter/js/script.js
+++ Assignments/module4-solution-starter/js/script.js
@@ -102,11 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-      var chosenCategoryShortName = function(
-        chooseRandomCategory(categories),responseData
-        );
-       chosenCategoryShortName;
-      //chooseRandomCategory(categories);
+      chooseRandomCategory(categories);
       console.log(chosenCategoryShortName);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
